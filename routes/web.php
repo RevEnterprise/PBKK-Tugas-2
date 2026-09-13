4a6d0e1889c54f94856998f8e15ebd42fc43fd16
RevEnterprise <RevEnterprise@users.noreply.github.com>
2026-09-14 04:45:16 +0700

    final test

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,23 @@ Route::get('/', function () {
     return view('home');
 })->name('home');
 
+Route::get('/mahasiswa/{nrp}', function ($nrp) {
+    return view('mahasiswa', compact('nrp'));
+})->where('nrp', '[0-9]{10}')
+->name('mahasiswa');
+
+Route::get('/hitung-ipk/{ip1}/{ip2}', function ($ip1, $ip2) {
+    $jumlah = $ip1 + $ip2;
+    $rataRata = $jumlah / 2;
+
+    return view('ipk', compact(
+        'ip1',
+        'ip2',
+        'jumlah',
+        'rataRata'
+    ));
+})->name('hitung.ipk');
+
 Route::prefix('dashboard')->group(function () {
 
     Route::get('/mahasiswa/{nrp}', function ($nrp) {
@@ -24,6 +41,9 @@ Route::prefix('dashboard')->group(function () {
             'rataRata'
         ));
     })->name('hitung.ipk');
+    Route::get('/agent/{tema?}', function ($tema = null) {
+        return view('agent', compact('tema'));
+    })->name('agent');
 });
 
 Route::get('/agent/{tema?}', function ($tema = null) {
